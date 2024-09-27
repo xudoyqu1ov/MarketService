1a4a04f7aba8244ffe519c17ae3139b9e710d6a2
Abdulaziz <khudoyqulov.04@gmail.com>
2024-09-27 09:18:03 +0300

    Initial commit

---
+++ src/test/java/uz/pdp/marketservice/MarketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.marketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
